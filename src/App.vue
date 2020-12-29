ce3e0d5ced3a8677d1ee97faa4243282af77fc6e
tim-koehler <timkoehler97@live.de>
2020-12-29 12:15:46 +0100

    working...

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view />
+  <Header></Header>
+  <router-view :lobby="lobby" :model="model"></router-view>
+  <footer-component></footer-component>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Header from "@/components/Header.vue"; // @ is an alias to /src
+
+export default defineComponent({
+  name: "App",
+  components: {
+    Header
+  }
+});
+</script>
 
 <style>
 #app {
---
+++ src/components/Header.vue
@@ -0,0 +1,77 @@
+<template>
+  <nav class="navbar navbar-expand-md navbar-light fixed-top" id="header">
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarCollapse"
+      aria-controls="navbarCollapse"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div
+      class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2"
+      id="navbarCollapse"
+    >
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="/"
+            >Main Menu <span class="sr-only">(current)</span></a
+          >
+        </li>
+        <li class="nav-item active float-left">
+          <router-link class="nav-link" to="/about">About</router-link>
+        </li>
+        <li v-if="$store.getters.getGameRunning" class="nav-item active">
+          <router-link class="nav-link" to="/game">Save Game</router-link>
+        </li>
+        <li v-if="$store.getters.getGameRunning" class="nav-item active">
+          <a
+            id="undo"
+            class="nav-link"
+            v-on:click="callUndo"
+            href="javascript:void(0)"
+            >Undo</a
+          >
+        </li>
+        <li v-if="$store.getters.getGameRunning" class="nav-item active">
+          <a
+            id="redo"
+            class="nav-link"
+            v-on:click="callRedo"
+            href="javascript:void(0)"
+            >Redo</a
+          >
+        </li>
+      </ul>
+      <div
+        class="d-flex justify-content-center"
+        style="width: 100%; position: fixed; pointer-events: none"
+      >
+        <label class="title">ScotlandYard</label>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Header",
+  methods: {
+    callUndo: function () {
+      //this.$root.sendMessageOverWebsocket("undo")
+    },
+    callRedo: function () {
+      //this.$root.sendMessageOverWebsocket("redo")
+    },
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
